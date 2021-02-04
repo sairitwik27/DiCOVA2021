52af5209c52dbff70d3f78de2057b09e574c09f3
sairitwik27 <66405406+sairitwik27@users.noreply.github.com>
2021-02-05 00:37:59 +0530

    Add files via upload

---
+++ prep/softlinks.sh
@@ -0,0 +1,4 @@
+ln -s /home/oem/Desktop/kaldi/egs/aspire/s5/steps .
+ln -s /home/oem/Desktop/kaldi/egs/aspire/s5/utils .
+ln -s /home/oem/Desktop/kaldi/egs/aspire/s5/local .
+ln -s /home/oem/Desktop/kaldi/egs/aspire/s5/path.sh .
