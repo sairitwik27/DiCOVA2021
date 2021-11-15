7f3d383a30a2b72b029728bbb7c7c7b033ef4a40
Ritwik <66405406+sairitwik27@users.noreply.github.com>
2021-11-16 00:16:14 +0530

    Delete codes/features directory

--- codes/features/ark2numpy.py
+++
@@ -1,9 +0,0 @@
-import kaldiio
-import numpy as np
-PARENT_DIR = "/home/oem/Desktop/DiCOVA"
-FOLDER_PATH = "/home/oem/Desktop/DiCOVA/Coswara"
-with open(FOLDER_PATH+"/feats.scp",'r') as f:
-    for line in f:
-        (key,rxfile) = line.split(' ')
-        mfcc = kaldiio.load_mat(rxfile)
-        np.save(PARENT_DIR+"/feats/mfcc/"+key,mfcc)
